12a82dbb40c4f27e1707c90ec53577f6c22fbecc
galshaked <79583974+galshaked@users.noreply.github.com>
2025-03-24 21:20:23 +0200

    Update process_articles.py

--- process_articles.py
+++ process_articles.py
@@ -60,6 +60,7 @@ def process_articles():
     """Main function to process articles needing translation."""
     articles = get_main_articles()
     for article in articles:
+        print("main loop check")
         create_translation_records(article["Serial #"])  # Use the 'id' field from the main article
         update_main_article(article["Serial #"], {"translations_record_status": "Translations records created"})
 
