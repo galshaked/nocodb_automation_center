05826fbf86c86b125d0a02e0063edfe4f3d34087
galshaked <79583974+galshaked@users.noreply.github.com>
2025-03-24 21:51:44 +0200

    Update process_articles.py

--- process_articles.py
+++ process_articles.py
@@ -49,6 +49,7 @@ def create_translation_records(article_serial):
             "Main Article": article_serial,  # Linking to the main article
             "Language Code": lang,  # Assigning language
         }
+        print(payload)
         url = f"{NOCO_BASE_URL}/tables/{TRANSLATIONS_TABLE}/records"
         requests.post(url, headers=HEADERS, json=payload)
 
